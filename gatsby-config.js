eb45de20d1a5e6fbb4847140ac1322d164e8dad9
Rasmus Nordling <rasmus.nordling@gmail.com>
2023-12-20 21:44:23 +0100

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,12 @@
  */
 module.exports = {
   siteMetadata: {
+    title: `Title of my Website`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     `gatsby-plugin-pnpm`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`, // some weird error
   ],
-}
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>This website is made with Gatsby.</p>
+      <StaticImage
+        alt="My alt text."
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
+      <StaticImage
+        alt="This is a local image"
+        src="../images/rachel-coyne-unsplash.jpeg"
+      />
     </Layout>
   );
 };
